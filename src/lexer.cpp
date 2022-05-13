8502891c20b23ecb6805208ebb72afb7278b2173
Pbotsaris <pedro@antfood.com>
2022-05-13 06:14:25 +0200

    added partial lexer

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -1,4 +1,4 @@
-#include "../include/validator.hpp"
+#include "../include/lexer.hpp"
 
 RegexPair CLIENT      (client, std::regex("^[a-zA-Z1-9]+"));
 RegexPair PROJECT     (project, std::regex("^[a-zA-Z1-9]+"));
@@ -11,7 +11,31 @@ RegexPair TS          (ts, std::regex("^\\d{1,2}-\\d{1,2}$"));
 RegexPair COMPOSER    (composer, std::regex("^[a-zA-Z]{2,3}$"));
 RegexPair TALENT      (talent, std::regex("^[a-zA-Z]+.[a-zA-Z]+$"));
 
+Token::Token(field_type type, std::string &value):
+m_type(type),
+m_value(std::move(value))
+{}
 
-Validator::Validator() :
-m_regexes({CLIENT, PROJECT, NAME, DESCRIPTOR, DATE, KEY, BPM, TS, COMPOSER, TALENT})
+Token::Token(field_type type, const char *value):
+m_type(type),
+m_value(value)
+{}
+
+
+Lexer::Lexer(const std::string &filename) :
+m_regexes({CLIENT, PROJECT, NAME, DESCRIPTOR, DATE, KEY, BPM, TS, COMPOSER, TALENT}),
+m_filename(filename),
+m_delimiter("_"),
+m_cursor(0),
+m_length(filename.length())
 { };
+
+
+Token Lexer::next_token()
+{
+  //std::string res = m_filename.substr(0, m_filename.find(m_delimiter));
+
+  if(m_cursor == m_length)
+      return Token(end_of_line, "");
+        
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include "../include/crawler.hpp"
+#include "../include/lexer.hpp"
 
 int main()
 {
-  Crawler crawler;
-  crawler.read_directory_tree();
+ // Crawler crawler;
+ // crawler.read_directory_tree();
+
+  Lexer lexer("client_project_name_102012");
+
+  lexer.next_token();
 
   return 0;
 }
